5979b8612ab08ab31d591ba7c4545e4ddc69418d
Cristian Enguídanos Nebot <53697395+cenguidanos@users.noreply.github.com>
2022-12-04 00:38:32 +0100

    refactor: lines

--- api/Contexts/Ecommerce/Store/Application/Command/CreateProduct.cs
+++ api/Contexts/Ecommerce/Store/Application/Command/CreateProduct.cs
@@ -6,13 +6,9 @@ namespace api.Contexts.Ecommerce.Store.Application.Command
     public class CreateProductCommand : IRequest<string>
     {
         public required string Id { get; set; }
-
         public required int Price { get; set; }
-
         public required string Title { get; set; }
-
         public required string Description { get; set; }
-
         public required int Status { get; set; }
     }
 
